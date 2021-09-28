1959c9f48d5441948c2937ff095e0f3be8bebdd5
Richard Reeve <git@richardreeve.net>
2021-09-28 10:43:17 +0100

    Update simple model to output java specific name and use new FDP_LOCAL_TOKEN enironment variable.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,22 +6,21 @@ public class main {
             System.err.println("Usage: gradle run {seirsFromPrepared,seirsFromExternal,prepare} config script");
             System.exit(1);
         }
-        if(System.getenv("REGTOKEN") == null) {
-            System.err.println("You need to set the environment variable REGTOKEN to the authorization token for the local registry");
+        if(System.getenv("FDP_LOCAL_TOKEN") == null) {
+            System.err.println("The fair CLI needs to set the environment variable FDP_LOCAL_TOKEN to the authorization token for the local registry");
             System.exit(2);
         }
         if(args[0].equals("prepare")) {
             PrepareParams pp = new PrepareParams();
-            pp.run(Path.of(args[1]), Path.of(args[2]), System.getenv("REGTOKEN"));
+            pp.run(Path.of(args[1]), Path.of(args[2]), System.getenv("FDP_LOCAL_TOKEN"));
         }else if(args[0].equals("seirsFromPrepared")) {
             SEIRS s = new SEIRS();
-            s.runFromPrepared(Path.of(args[1]), Path.of(args[2]), System.getenv("REGTOKEN"));
+            s.runFromPrepared(Path.of(args[1]), Path.of(args[2]), System.getenv("FDP_LOCAL_TOKEN"));
         }else if(args[0].equals("seirsFromExternal")) {
             SEIRS s = new SEIRS();
-            s.runFromExternal(Path.of(args[1]), Path.of(args[2]), System.getenv("REGTOKEN"));
+            s.runFromExternal(Path.of(args[1]), Path.of(args[2]), System.getenv("FDP_LOCAL_TOKEN"));
         }else{
             System.err.println("first argument must be 'seirsFromExternal', 'seirsFromPrepared' or 'seirs'");
         }
     }
-
 }
